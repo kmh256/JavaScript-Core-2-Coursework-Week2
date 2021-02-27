832d5857910d41606bcd10213ccf9c5c19306328
Kara Howard <kara.mh191@gmail.com>
2021-02-27 13:55:11 +0000

    Exercise B - Shopping List
    Exercise B - Shopping List complete.

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,6 +1,25 @@
-function shoppingList(arrayOfPeople) {
-  // Write your code here...
-}
+function shoppingList(arrayOfItems) {
+
+  let body = document.querySelector("body");
+  // Creates h1 element inside body
+  let h1Tag = document.createElement("h1");
+  h1Tag.innerText = `My Shopping List`;
+  // Adds h1Tag as first child
+  body.prepend(h1Tag);
+
+  // * You should use an unordered list: `<ul>`. Creates a ul tag.
+  let ulTag = document.createElement("ul");
+
+  // * Each item from the array should be in a `<li>` tag. Creates a li tag, adds each item in shopping list as an li element, adds the li elements as children of ul.
+  for (item of arrayOfItems) {
+    let liTag = document.createElement("li");
+    liTag.innerText = item;
+    ulTag.appendChild(liTag);
+  };
+
+  // * All of your HTML should go inside the `<div>` with the id **"content"**. Adds ul element as a child of the div with id "content".
+  content.appendChild(ulTag);
+};
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
 
