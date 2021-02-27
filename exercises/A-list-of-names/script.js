323cf22fd13a74e45f56d25d23c2644fae91b77f
Kara Howard <kara.mh191@gmail.com>
2021-02-27 13:04:15 +0000

    Exercise A - List of Names
    Exercise A - List of Names complete.

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,6 +1,21 @@
 function listOfNames(arrayOfPeople) {
   let content = document.querySelector("#content");
-}
+
+  for (let person of arrayOfPeople) {
+
+    // - Add a `<h1>` tag to the html containing the name of the person. Creates h1 element then changes inside of tags to names in object
+    let h1Tag = document.createElement("h1");
+    h1Tag.innerText = `Name: ${person.name}`;
+
+    // - Add a `<h2>` tag to the html containing the job of the person. Creates h2 element then changes inside of tags to jobs in object
+    let h2Tag = document.createElement("h2");
+    h2Tag.innerText = `Job: ${person.job}`;
+
+    // - All of your HTML should go inside the `<div>` with the id **"content"**: Adds h1Tag as a child node of the div with id "content"
+    content.appendChild(h1Tag);
+    content.appendChild(h2Tag);
+  }
+};
 
 let people = [
   { name: "Chris", job: "Teacher" },
