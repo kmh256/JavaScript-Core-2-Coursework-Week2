417349343831a95ed1d462757e7c8f8a1b50ab99
Kara Howard <kara.mh191@gmail.com>
2021-02-27 16:55:32 +0000

    Exercise D - Todo List
    Exercise D - Todo List complete

--- exercises/D-todo-list/script.js
+++ exercises/D-todo-list/script.js
@@ -1,5 +1,36 @@
 function todoList(todos) {
-  // Write your code here...
+
+  // Creates h1 element inside body
+  let body = document.querySelector("body");
+  let h1Tag = document.createElement("h1");
+  h1Tag.innerText = "Todo List";
+  content.appendChild(h1Tag);
+  // Creates ul element inside body
+  let ulTag = document.createElement("ul");
+  body.appendChild(ulTag);
+
+  for (let job of todos){
+    // Creates li tag for each job and adds as a child to ulTag
+    let liTag = document.createElement("li");
+    liTag.innerText = job.todo;
+    ulTag.appendChild(liTag);
+
+    // * Each todo should have an event listener for when they are clicked.
+    liTag.addEventListener("click", lineThrough);
+
+    function lineThrough() {
+      // * The event listener should add the line-through style to the todo.
+      if (liTag.style.textDecoration === "line-through") {
+        liTag.style.textDecoration = "none";
+        // The event listener should also remove the line-through style conditionally by checking if the style is already applied.
+      } else {
+        liTag.style.textDecoration = "line-through";
+      }
+    }
+  }
+
+  // * All of your HTML should go inside the `<div>` with the id **"content"**.
+  content.appendChild("ulTag");
 }
 
 const todos = [
